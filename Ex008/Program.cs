896573331e6f54212c66662d5782896bec12a38d
Ventru67 <ventru67@gmail.com>
2023-01-15 20:03:22 +0300

    Eighth task done

---
+++ Ex008/Program.cs
@@ -0,0 +1,11 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.Write("Введите трёзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int first = (number / 100) * 10;
+int last = number % 10;
+int result = first + last;
+
+Console.WriteLine(result);
+
+
