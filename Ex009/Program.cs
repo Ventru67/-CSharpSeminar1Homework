24e379bf3892a82ad2b7063e26cbdb684d3bb88a
Ventru67 <ventru67@gmail.com>
2023-01-15 20:38:53 +0300

    Nineth task done

---
+++ Ex009/Program.cs
@@ -0,0 +1,19 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Write("Введите кратное число: ");
+int firstNumber = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите делитель: ");
+int secondNumber = int.Parse(Console.ReadLine() ?? "0");
+
+int remainder = firstNumber % secondNumber;
+if (firstNumber < secondNumber) 
+{
+    Console.WriteLine("Кратное должно быть больше делителя");
+}
+else if (remainder == 0)
+{
+    Console.WriteLine("Второе число кратно первому");
+}
+else {
+    Console.WriteLine("Второе число не кратно первому, остаток: " + remainder);
+}
