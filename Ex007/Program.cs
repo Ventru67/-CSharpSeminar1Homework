d9f3b26d53900bc9e8b41d124a97a53064056ade
Ventru67 <ventru67@gmail.com>
2023-01-15 19:46:38 +0300

    Seventh task done

---
+++ Ex007/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.Write("Введите число из отрезка [10, 99]: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int x = number % 10;
+int y = number / 10;
+
+if (x >= y)
+{
+    Console.WriteLine(x);
+} 
+if (x < y)
+{
+    Console.WriteLine(y);
+} 
