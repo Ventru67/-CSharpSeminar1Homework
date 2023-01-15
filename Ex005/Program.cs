2854b87d6d46b02a509562a86142447b2adb93ef
Ventru67 <ventru67@gmail.com>
2023-01-15 19:28:45 +0300

    Fifth task done

---
+++ Ex005/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.Write("Введите трёзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int result = number % 10;
+Console.WriteLine(result);
