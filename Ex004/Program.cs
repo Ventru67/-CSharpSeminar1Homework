7472e2fdd8aa356abc628788fb480831207ed4cf
Ventru67 <ventru67@gmail.com>
2023-01-15 19:20:51 +0300

    Fourth task done

---
+++ Ex004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать четные числа от 1 до N
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int nextNumber = 1;
+
+while (nextNumber <= number)
+{
+    if (nextNumber % 2 == 0)
+        Console.WriteLine(nextNumber);
+        nextNumber++; 
+}
