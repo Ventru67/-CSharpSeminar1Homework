52bdd2da24b69285be858e4cea1189d918c0feb9
Ventru67 <ventru67@gmail.com>
2023-01-15 21:48:06 +0300

    Nineth task done

---
+++ Ex010/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number < 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{ 
+    while (number >= 1000) number /= 10;
+    {
+    int result = number % 10;
+    Console.WriteLine(result);
+    }
+}
