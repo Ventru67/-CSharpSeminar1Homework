dd93e111f92e042c98f3f994354be15ed4e2ade1
Ventru67 <ventru67@gmail.com>
2023-01-15 19:33:29 +0300

    Sixth task done

---
+++ Ex006/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите трёзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int result = number % 100 / 10;
+Console.WriteLine(result);
